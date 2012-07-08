50d69342101fc9d17b5cbb935b6c12a7ec0a9911
Chris Goulart <cghouly@gmail.com>
2012-07-08 16:25:05 -0700

    Added device_type, os_version, os, and key to the database

--- models.rb
+++ models.rb
@@ -6,6 +6,10 @@ class Device
   include MongoMapper::Document
 
   key :name, String, :required => true
+  key :device_type, String, :required => false
+  key :os_version, String, :required => false
+  key :os, String, :required => false
+  key :key, String, :required => false
   key :missing, Boolean, :default => false
   key :delay, Integer, :default => 20, :in => 2..59
   key :module_list, String, :default => 'geo network session webcam'
